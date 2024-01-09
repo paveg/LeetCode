57bb9d6cdb001752341570b468c2fb64e28e4d53
Ryota Ikezawa <pavegy@gmail.com>
2024-01-09 23:19:03 +0900

    :sparkles: Solved 169

---
+++ easy/169.majority-element.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 0
+        return max(d, key=d.get)
+
+# @lc code=end
