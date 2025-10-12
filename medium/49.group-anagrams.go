950fe3fafba583af840079da27ebe89bee8fb000
Ryota Ikezawa <pavegy@gmail.com>
2025-10-12 23:06:28 +0900

    :recycle: Add improved version code

--- medium/49.group-anagrams.go
+++ medium/49.group-anagrams.go
@@ -1,7 +1,6 @@
 package code
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -13,30 +12,48 @@ import (
 
 // @lc code=start
 func groupAnagrams(strs []string) [][]string {
-	m := map[string]int{}
-	anagrams := [][]string{}
-
-	i := 0
-
+	// O(n * k log k)
+	groups := make(map[string][]string, len(strs))
 	for _, s := range strs {
-		sorted := strSort(s)
-		v, ok := m[sorted]
-		if ok {
-			// exists
-			anagrams[v] = append(anagrams[v], s)
-		} else {
-			anagrams = append(anagrams, []string{s})
-			m[sorted] = i
-			i++
-		}
+		k := sortStringASCII(s)
+		// groups["aet"] = ["eat", "tea", "ate"]
+		groups[k] = append(groups[k], s)
 	}
 
-	return anagrams
+	// map to 2d slice
+	res := make([][]string, 0, len(groups))
+	for _, g := range groups {
+		res = append(res, g)
+	}
+
+	return res
 }
 
-func strSort(s string) string {
+// func groupAnagramsWithfrequencyArrayKey(strs []string) [][]string {
+// 	// O(n * k)
+// 	groups := make(map[[26]byte][]string, len(strs))
+// 	for _, s := range strs {
+// 		var key [26]byte
+// 		for i := 0; i < len(s); i++ {
+// 			//
+// 			key[s[i]-'a']++
+// 		}
+// 		groups[key] = append(groups[key], s)
+// 	}
+
+// 	res := make([][]string, 0, len(groups))
+// 	for _, g := range groups {
+// 		res = append(res, g)
+// 	}
+
+// 	return res
+// }
+
+func sortStringASCII(s string) string {
 	b := []byte(s)
-	sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+	sort.Slice(b, func(i, j int) bool {
+		return b[i] < b[j]
+	})
 	return string(b)
 }
 
