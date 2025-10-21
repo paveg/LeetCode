515b9d25787f25478cd2e8f94d4db02bfa2a4893
Ryota Ikezawa <pavegy@gmail.com>
2025-10-22 03:01:52 +0900

    :recycle: fixed

---
+++ easy/2765.longest-alternating-subarray.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=2765 lang=golang
+ *
+ * [2765] Longest Alternating Subarray
+ */
+
+// @lc code=start
+func alternatingSubarray(nums []int) int {
+    
+}
+// @lc code=end
+
